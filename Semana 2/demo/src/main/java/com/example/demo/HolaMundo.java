a6157abdbddfd9247771aa78db024f6fedfeca68
kelocoes <kevin.rodriguez109@gmail.com>
2025-02-08 15:49:48 -0500

    Update first week

---
+++ Semana 2/demo/src/main/java/com/example/demo/HolaMundo.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class HolaMundo {
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String message;
+}
