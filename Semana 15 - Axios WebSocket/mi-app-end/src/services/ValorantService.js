9f821b68cf7e6b729857802e80cfb6c122ff8dfc
kelocoes <kevin.rodriguez109@gmail.com>
2025-05-22 14:57:33 -0500

    Adding axios

--- Semana 15 - Axios WebSocket/mi-app-end/src/services/ValorantService.js
+++ Semana 15 - Axios WebSocket/mi-app-end/src/services/ValorantService.js
@@ -1,9 +1,9 @@
+import { axiosInstance } from "./axios";
 
 const getAgents = async () => {
     try {
-        const response = await fetch('https://valorant-api.com/v1/agents');
-        const data = await response.json();
-        return data.data;
+        const response = await axiosInstance('/v1/agents');
+        return response.data.data;
     } catch (error) {
         console.error('Error fetching agents:', error);
         throw error;
